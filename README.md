63f23678b3649a3739fd76b19f901b9c1fd505b2
Suvid <suviddevguglani@gmail.com>
2023-09-20 04:18:58 +0530

    All Files Added

---
+++ README.md
@@ -0,0 +1,7 @@
+# PORTFOLIO WEBSITE
+
+This is my personal portfolio website built using HTML, CSS, and JavaScript. It showcases my projects, skills, and provides an overview of my journey as a developer.
+
+
+WEBSITE LINK:
+https://guglanisuvid.github.io/Portfolio-Website/
---
+++ js/script.js
@@ -0,0 +1,13 @@
+const hambrg = document.querySelector("menu");
+const hamBars = '<i class="bi bi-list"></i>';
+const hamX = '<i class="bi bi-x"></i>';
+const navBar = document.getElementById("mob-nav-items");
+function navToggle() {
+    if (hambrg.innerHTML == hamBars) {
+        hambrg.innerHTML = hamX;
+    } else {
+        hambrg.innerHTML = hamBars;
+    }
+
+    navBar.classList.toggle("visible");
+}
